1e4db754e85daf1d61575eeb7d3115f8d49e427c
ArtemProhodtsev <art.pro.tms@gmail.com>
2021-06-22 20:25:20 +0300

    Переделал домашнее заданее №2 состоящее из 5 заданий. Работа с методами. В предыдущих решениях не было работы с данными. Точнее она была, но вводом из консоли.

--- Lessons2/src/Task2.java
+++ Lessons2/src/Task2.java
@@ -1,17 +1,18 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class test {
+public class Task2 {
     public static void main(String[] args) {
-        int s = summa();
+        int s = summa(10, -90);
         System.out.println("По условию задачи 1 метод summa() нам вернул:" + " " + s);
-        int m = max();
+
+        int m = max(10, 40);
         System.out.println("По условию задачи 2 метод max() нам вернул:" + " " + m);
-        double ave = average();
+
+        double ave = average(new int[]{0, -2, 3, -1, 5});
         System.out.println("Cреднее значение по задаче 3 из массива чисел:" + " " + ave);
-        int maxArray = arrmax();
+
+        int maxArray = arrmax(new int[]{10, 45, 3, 10, 5});
         System.out.println("Максимальный элемент по задаче 4 из массива чисел:" + " " + maxArray);
-        double hyp = calculateHypotenuse();
+
+        double hyp = calculateHypotenuse(12,15);
         String formattedHyp = String.format("%.2f", hyp);
         System.out.println("Значение гиппотенузы по задаче 5:" +" "+formattedHyp);
     }
@@ -20,32 +21,20 @@ public class test {
      * 1. Метод должен вернуть сумму двух чисел a и b
      * 2. Дополнительно: сделать проверку если сумма a и b больше чем максимальное значение int то вернуть -1
      **/
-    public static int summa() {
-        System.out.println("<=========Задача 1 из 5 (сумму двух чисел с условием)=========>");
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print("Введите число а:");
-        int a = keyboard.nextInt();
-        System.out.print("Введите число b:");
-        int b = keyboard.nextInt();
+    public static int summa(int a, int b) {
         int summaAB = a + b;
         int maxAB = Math.max(a, b);
         if (summaAB > maxAB) {
             return (-1); //
         } else {
-            return (summaAB);
+            return summaAB;
         }
     }
 
     /**
      * Метод должен вернуть максимальное значение из двух чисел
      **/
-    public static int max() {
-        System.out.println("<=========Задача 2 из 5 (максимальное значение)=========>");
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print("Введите число а:");
-        int a = keyboard.nextInt();
-        System.out.print("Введите число b:");
-        int b = keyboard.nextInt();
+    public static int max(int a, int b) {
         int maxAB = Math.max(a, b);
         return (maxAB);
     }
@@ -53,59 +42,34 @@ public class test {
     /**
      * Метод должен вернуть среднее значение из массива чисел
      **/
-    public static double average() {
-        System.out.println("<=========Задача 3 из 5 (среднее значение)=========>");
-        System.out.print("Введите с консоли через пробел числа:");
-        Scanner keyboard = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<>();
-        String s = keyboard.nextLine();
-        keyboard = new Scanner(s);
-        while (keyboard.hasNextInt()) {
-            arr.add(keyboard.nextInt());
-        }
-        double average = 0;
-        if (arr.size() > 0) {
-            double sumArray = 0;
-            for (int i = 0; i < arr.size(); i++) {
-                sumArray += arr.get(i);
+    public static double average(int[] array) {
+        double sum = 0;
+        double count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                sum += array[i];
+                count++;
             }
-            average = sumArray / arr.size();
         }
-        return (average);
+        return sum/count;
     }
-
     /**
      * Метод должен вернуть максимальый элемент массива.
      **/
-    public static int arrmax() {
-        System.out.println("<=========Задача 4 из 5 (максимальный элемент массива)=========>");
-        System.out.print("Введите с консоли через пробел числа:");
-        Scanner keyboard = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<>();
-        String s = keyboard.nextLine();
-        keyboard = new Scanner(s);
-        while (keyboard.hasNextInt()) {
-            arr.add(keyboard.nextInt());
-        }
-        int maxArr = arr.get(0);
-        for (int i = 1; i < arr.size(); i++)
-            maxArr = Math.max(maxArr, arr.get(i));
-        return (maxArr);
+    public static int arrmax(int[] array) {
+
+        int maxArr = array[0];
+        for (int i = 1; i < array.length; i++)
+            maxArr = Math.max(maxArr, array[i]);
+        return maxArr;
     }
 
     /**
      * Используя теорему Пифагора, вычислите значение гипотенузы. Квадрат гипотенузы = сумме квадратов катетов
      **/
-    public static double calculateHypotenuse() {
-        System.out.println("<=========Задача 5 из 5 (значение гипотенузы)=========>");
-        System.out.print("Введите значение для первого катета:");
-        Scanner keyboard = new Scanner(System.in);
-        double katet1 = keyboard.nextInt();
-        System.out.print("Введите значение для второго катета:");
-        double katet2 = keyboard.nextInt();
+    public static double calculateHypotenuse(double katet1, double katet2) {
         double hypothenuse = Math.sqrt((katet1 * katet1) + (katet2 * katet2));
-        return (hypothenuse);
-
+        return hypothenuse;
     }
 }
 
