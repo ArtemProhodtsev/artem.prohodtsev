df9a9edfd09ee1d4eb1611d3168f83962bdfd648
ArtemProhodtsev <art.pro.tms@gmail.com>
2021-06-24 11:06:08 +0300

    первая задача: добавл работу по переводу int в long для переменной b, добавлено примедение возвращаемой переменной summaAB к int четвёртая задача: добавлена проверка на ввод массива не содержащего элементы, в этом случае система возвращает 0. Пытался сделать так чтобы такое исключение обрабатывалось выводом сообщения "Введён пустой массив", else { return ("Введён пустой массив"); } но получилось это сделать только public static serialize () но показалось полной лажой...

--- Lessons2/src/Task2.java
+++ Lessons2/src/Task2.java
@@ -3,34 +3,33 @@ import java.util.stream.IntStream;
 public class Task2 {
     public static void main(String[] args) {
         long s = summa(10, -90);
-        System.out.println("По условию задачи 1 метод summa() нам вернул:" + " " + s);
+        System.out.println("По условию задачи 1 метод summa() нам вернул: " + s);
 
         int m = max(10, 40);
-        System.out.println("По условию задачи 2 метод max() нам вернул:" + " " + m);
+        System.out.println("По условию задачи 2 метод max() нам вернул: " + m);
 
         double ave = average(new int[]{0, -2, 3, -1, 5});
-        System.out.println("Cреднее значение по задаче 3 из массива чисел:" + " " + ave);
+        System.out.println("Среднее значение по задаче 3 из массива чисел: " + ave);
+        System.out.println(getMax(new int[]{}));
 
-        System.out.println(arrmax(new int[]{}));
+        int maxArray = getMax(new int[]{10, 45, 3, 10, 5});
+        System.out.println("Максимальный элемент по задаче 4 из массива чисел: " + maxArray);
 
-        int maxArray = arrmax(new int[]{10, 45, 3, 10, 5});
-        System.out.println("Максимальный элемент по задаче 4 из массива чисел:" + " " + maxArray);
         double hyp = calculateHypotenuse(12, 15);
         String formattedHyp = String.format("%.2f", hyp);
-        System.out.println("Значение гиппотенузы по задаче 5:" + " " + formattedHyp);
+        System.out.println("Значение гиппотенузы по задаче 5: " + formattedHyp);
     }
 
     /**
      * 1. Метод должен вернуть сумму двух чисел a и b
      * 2. Дополнительно: сделать проверку если сумма a и b больше чем максимальное значение int то вернуть -1
      **/
-    public static long summa(long a, long b) {
-        long summaAB = a + b;
-        long maxAB = Math.max(a, b);
-        if (maxAB > Integer.MAX_VALUE) {
+    public static int summa(int a, int b) {
+        long summaAB = a + (long) b;
+        if (summaAB > Integer.MAX_VALUE) {
             return -1; //
         } else {
-            return summaAB;
+            return (int) summaAB;
         }
     }
 
@@ -53,27 +52,26 @@ public class Task2 {
     /**
      * Метод должен вернуть максимальый элемент массива.
      **/
-    public static int arrmax(int[] array) {
-        int maxArr = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array.length == 0) {
-                System.out.println("Массив пришёл на вход пустой");
-            } else {
-                maxArr = Math.max(maxArr, array[i]);
+    public static int getMax(int[] inputArray) {
+        if (inputArray.length != 0) {
+            int maxValue = inputArray[0];
+            for (int i = 1; i < inputArray.length; i++) {
+                if (inputArray[i] > maxValue) {
+                    maxValue = inputArray[i];
+                }
             }
+            return maxValue;
+        } else {
+            return (0);
         }
-        return (maxArr);
     }
-
-
 
     /**
      * Используя теорему Пифагора, вычислите значение гипотенузы. Квадрат гипотенузы = сумме квадратов катетов
      **/
 
     public static double calculateHypotenuse(double katet1, double katet2) {
-        double hypothenuse = Math.sqrt((katet1 * katet1) + (katet2 * katet2));
-        return hypothenuse;
+        return Math.sqrt((katet1 * katet1) + (katet2 * katet2));
     }
 }
 
