67dd7a600c5f625383308a9741cd2a729af12ee2
ArtemProhodtsev <art.pro.tms@gmail.com>
2021-08-04 22:44:28 +0300

    HomeWork13 task1, task2, task3 подключал библиотеку Apache Commons Lang 3 прописывая в pom. С ней дальше и работал.

---
+++ Lessons13/src/main/java/Main.java
@@ -0,0 +1,48 @@
+/**
+ * 1) Вырезать подстроку из строки начиная с первого вхождения символа(А) до, последнего вхождения сивола(B).
+ * 2) Заменить все вхождения символа стоящего в позиции (3) на символ стоящий в позиции 0
+ * 3) В массиве находятся слова. Вывести на экран слова палиндромы (т.е которые читаются справа на лево и слева на право одинаково, например: заказ, казак, дед...)
+ * 4) Есть строка в которой содержится текст, предложения разделены точками. После запуска программы на экран должны выводиться только те предложения в которых от 3-х до 5-ти слов.
+ * Если в предложении присутствует слово-палиндром, то не имеет значения какое количество слов в предложении, оно выводится на экран.
+ * Пишем все в ООП стиле. Создаем класс TextFormatter
+ * в котором два статических метода:
+ * 1. Метод принимает строку и возвращает кол-во слов в строке.
+ * 2. Метод принимает строку и проверяет есть ли в строке слово-палиндром. Если есть возвращает true, если нет false
+ * В main создаем строку с текстом, также можно текст задавать с консоли.
+ * Разбиваем текст на предложения. Используя методы класса TextFormatter определяем подходит ли нам предложение.
+ * Если подходит, то выводим на экран.
+ */
+
+import com.artpro.service.Text;
+import com.artpro.service.TextImpl;
+
+import static org.apache.commons.lang3.StringUtils.reverse;
+
+public class Main {
+    private static Text textFormatter = new TextImpl();
+
+    public static void main(String[] args) {
+        System.out.println(task1("мАрка, Варка"));
+        System.out.println(task2("ночь"));
+        task3("ночь, заказ, казак, дед");
+    }
+
+    private static String task1(String text) {
+        int firstIndex = text.indexOf('А');
+        int secondIndex = text.lastIndexOf('В');
+        return text.substring(firstIndex, secondIndex);
+    }
+
+    private static String task2(String text) {
+        return text.replace(text.charAt(3), text.charAt(0));
+    }
+
+    private static void task3(String text) {
+        String[] words = ((TextImpl) textFormatter).Split(text);
+        for (String word : words) {
+            if (word.equals(reverse(word)) && word.length() > 1) {
+                System.out.println(word);
+            }
+        }
+    }
+}
---
+++ Lessons13/src/main/java/com/artpro/service/Text.java
@@ -0,0 +1,7 @@
+package com.artpro.service;
+
+public interface Text {
+    int Count(String text);
+    boolean Palindrome(String text);
+}
+
